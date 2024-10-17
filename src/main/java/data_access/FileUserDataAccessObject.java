29ac47d65be7e668b9e3f6e7a4933e9e536d3c0b
Anh Dang Phuong <anh.dangphuong@mail.utoronto.ca>
2024-10-16 21:26:46 -0400

    Implement Task 2.1

--- src/main/java/data_access/FileUserDataAccessObject.java
+++ src/main/java/data_access/FileUserDataAccessObject.java
@@ -103,4 +103,8 @@ public class FileUserDataAccessObject implements SignupUserDataAccessInterface,
         accounts.put(user.getName(), user);
         save();
     }
+
+    @Override
+    public void setCurrentUser(String name) {
+    }
 }
